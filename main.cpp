608ee6387783620be6412e80f3fb32e56e114602
VManuelSM <vctr.mnl.s.m@gmail.com>
2018-06-19 07:00:00 -0500

    Victor Manuel Santos Martinez

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ main()
 	Kevin k = Kevin("Kevin");
 	mario msp = mario("Sevilla Ponce Mario", 1731114528);
 	Rogelio ruvc = rogelio("Vergara Covarrubias Rogelio Uriel",1731114458);
+	Victor v = Victor("Victor");
 	/*Imprimir su nombre*/
 	cout<<"Objeto con nombre "<<k.getNombre()<<endl;
+	cout<<"Objeto con nombre "<<V.getNombre()<<endl;
 	cout<<"Objeto con nombre: "<<msp.getNombre()+" Matricula :"<<msp.getMatricula()<<endl;
 	//Rogelio
 	cout<<"Objeto con nombre:"<<ruvc.getNombre()+" Matricula: "<<ruvc.getMatricula()<<endl;
